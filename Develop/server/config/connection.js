de8bc4475ec21e81cc4c8f74f4da4a463f806e95
Xandromus <xanderrapstine@gmail.com>
2021-06-04 16:30:14 -0500

    add in apollo changes

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -4,7 +4,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: true,
+  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
