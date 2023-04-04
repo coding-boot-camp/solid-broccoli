2c59c39ec474afd8470f4ce082553c247452f955
georgeyoo <gcyoosf@gmail.com>
2023-04-04 12:28:16 -0400

    updates to use mongoose v7 and alters searchBooks return

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
